a866d2f46ab8b6f8ea0b45bc6adb960c80d69d85
BSord <brandon.sorgdrager@gmail.com>
2019-02-17 23:58:17 +0000

    Major performance improvements, rewrote core functions, modified mouse event handlers

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,57 +7,96 @@ class Board extends Component {
     super();
     this.state = {
       boardState: false,
-      color: '#FFF'
+      color: '#FFF',
+      pendingChanges: []
     };
   }
 
   componentDidMount() {
-		const { socket} = this.props;
-    //use the below to
-		//socket.emit("update",  "1");
+    // DEFINE SOCKET EVENT LISTENERS
+
+    const { socket} = this.props;
+   
     socket.on("setBoardState", receivedState => {
-      //console.log("boardState received:" + JSON.stringify(receivedState));
       this.setState({boardState: receivedState});
       console.log(receivedState.connections);
     })
+
+    socket.on("updateTiles", tileUpdateData => {
+      var desiredState = {...this.state.boardState}
+      for (var i = 0; i < tileUpdateData.length; i++ ){
+        desiredState.tiles[tileUpdateData[i].x][tileUpdateData[i].y] = tileUpdateData[i].color 
+      }
+      this.setState({boardState: desiredState});
+    })
+
 	}
 
-  changeTileColor(x,y) {
+  changeTileColor(x,y,e) {
       //do some stuff
-      const { socket} = this.props;
       var desiredState = {...this.state.boardState}
       var {color} = this.state;
+      var tileUpdateData = {}
+      tileUpdateData.x = x
+      tileUpdateData.y = y
+      tileUpdateData.color = color
 
       if (desiredState.tiles[x][y] === color){
         desiredState.tiles[x][y] = desiredState.baseColor;
-        this.setState({boardState: desiredState});
-        socket.emit("updateBoardState",  desiredState);
+        tileUpdateData.color = desiredState.baseColor
+        //this.setState({boardState: desiredState});
+        this.state.pendingChanges.push(tileUpdateData);
+        //socket.emit("updateTile", tileUpdateData)
       } else {
         desiredState.tiles[x][y] = color;
-        this.setState({boardState: desiredState});
-        socket.emit("updateBoardState",  desiredState);
+        //this.setState({boardState: desiredState});
+        this.state.pendingChanges.push(tileUpdateData);
+        //socket.emit("updateTile", tileUpdateData)
       }
   }
 
   changeTileColorMouseMove(x,y,e) {
     if(e.buttons === 1 || e.buttons === 3){
       //do some stuff
-      const { socket} = this.props;
       var desiredState = {...this.state.boardState}
       var {color} = this.state;
+      var tileUpdateData = {}
+      tileUpdateData.x = x
+      tileUpdateData.y = y
+      tileUpdateData.color = color
 
+      this.state.pendingChanges.push(tileUpdateData);
+      e.target.setAttribute("bgColor", color);
       desiredState.tiles[x][y] = color;
-      this.setState({boardState: desiredState});
-      socket.emit("updateBoardState",  desiredState);
+      //this.setState({boardState: desiredState});
+
       e.stopPropagation();
       e.preventDefault();
     }
-    
+  }
+
+  handleMouseUp(){
+    const { socket} = this.props;
+    console.log(this.state.pendingChanges);
+    socket.emit("updateTiles", this.selectUniqueChanges(this.state.pendingChanges))
+    this.setState({pendingChanges: []})
   }
 
   handleColorPicker = (color) => {
     this.setState({ color: color.hex });
   };
+
+  selectUniqueChanges(arr) {
+    var uniques = [];
+    var itemsFound = {};
+    for(var i = 0, l = arr.length; i < l; i++) {
+        var stringified = JSON.stringify(arr[i]);
+        if(itemsFound[stringified]) { continue; }
+        uniques.push(arr[i]);
+        itemsFound[stringified] = true;
+    }
+    return uniques;
+  }
 
   render() {
     const { boardState } = this.state;
@@ -71,7 +110,7 @@ class Board extends Component {
                   {boardState.tiles.map((row, i) =>
                     <tr key={i}>
                       {row.map((col, j) =>
-                        <td key={j} onMouseMove={(e)=>{ this.changeTileColorMouseMove(i,j,e) }} onMouseDown={(e)=>{ this.changeTileColor(i,j) }} bgcolor={col}></td>
+                        <td key={j} onMouseMove={(e)=>{ this.changeTileColorMouseMove(i,j,e) }} onMouseDown={(e)=>{ this.changeTileColor(i,j,e) }} onMouseUp={()=>this.handleMouseUp()} bgcolor={col}></td>
                       )}
                     </tr>
                   )}
